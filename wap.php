0338c0ecefbeea6e7c5edfb6693deb3838eb2b81
Roman <inozemtsev_roman@mail.ru>
2018-05-03 16:29:57 +1000

    function_show

--- wap.php
+++ wap.php
@@ -11,3 +11,34 @@ function waves_asset( $atts ) {
     return $demolph_output;
 }
 add_shortcode( "asset", "waves_asset" );
+
+function waves_show( $id,$fiat,$priceid ) { 
+    $response = wp_remote_get( "http://marketdata.wavesplatform.com/api/ticker/$id/$priceid" );
+    $body = wp_remote_retrieve_body( $response );
+$data = json_decode($body, TRUE);
+$cmcap = wp_remote_get("https://api.coinmarketcap.com/v1/ticker/waves/?convert=EUR");
+$cmbody = wp_remote_retrieve_body($cmcap);
+$cmdata = json_decode($cmbody, TRUE);
+    $asset = array_search("$id", array_column($data, 'amountAssetID'));
+    $price = $data['24h_close'];
+    $pricein = $data["priceAssetID"];
+    $pricename = $data["priceAssetName"];
+    $wprice = $cmdata[0]["price_usd"];
+    $btcprice = $cmdata[0]["price_btc"];
+     $eurprice = $cmdata[0]["price_eur"];
+     if ($pricein == "WAVES"){
+    if($fiat == "waves"){
+return $price;
+    }  elseif ($fiat == "btc"){
+        return number_format($price*$btcprice, 10);
+    } elseif ($fiat == "eur"){
+         return number_format($price*$eurprice, 10);
+    }
+    else{
+         return number_format($price*$wprice, 10);
+
+    }
+     } else {
+         return "$price $pricename";
+     }
+} 
